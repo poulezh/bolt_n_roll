95b44a39574aa5c8544abbf86161a93627bff4e5
poulezh <ezh1986@inbox.ru>
2021-08-15 22:32:52 +0700

    правки+3

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,14 @@ let inp_length = document.querySelector('.inp_length')
 let liste = document.querySelector('.liste')
 let res = document.querySelector('.res')
 let form = document.querySelector('.form')
+let btn_save = document.querySelector('.btn_save')
 
 reload.addEventListener('click', ()=>{
     location.reload()
 })
+
+
+
 
 function selectChangeOne() {
     let select = document.querySelector('.select_one').value   
@@ -56,19 +60,11 @@ function save(event) {
         liste.classList.add('hidden')
         form.classList.remove('hidden')
 
-        let name_title = document.querySelector('.name_title')
-        let length = document.querySelector('.length')
-        let thread = document.querySelector('.thread')
-        let standart = document.querySelector('.standart')
-        let strength = document.querySelector('.strength')
-        let steel = document.querySelector('.steel')
-        let coating = document.querySelector('.coating')
-        let standart_select = selectChangeOne()
-        let coating_select = selectChangeTwo()
-        let strength_select = selectChangeFour()
-        let steel_select = selectChangeThree()
-        if (standart_select === 'standart'){
-            standart.innerHTML = 'неопределено'
+        let select = {
+            'standart': selectChangeOne(),
+            'coating': selectChangeTwo(),
+            'steel': selectChangeThree(),
+            'strength': selectChangeFour()
         }
         let data = num_one()
         if (data == undefined) {
@@ -76,17 +72,17 @@ function save(event) {
             data.length = '0 mm'
             data.thread = '0 mm'
         }
-        name_title.innerHTML = data.name
-        thread.innerHTML = data.thread + 'mm'
-        length.innerHTML = data.length + 'mm'
-        standart.innerHTML = standart_select
-        strength.innerHTML = strength_select
-        steel.innerHTML = steel_select
-        coating.innerHTML = coating_select
+        document.querySelector('.name_title').innerHTML = data.name
+        document.querySelector('.thread').innerHTML = data.thread + 'mm'
+        document.querySelector('.length').innerHTML = data.length + 'mm'
+        document.querySelector('.standart').innerHTML = select.standart
+        document.querySelector('.strength').innerHTML = select.strength
+        document.querySelector('.steel').innerHTML = select.steel
+        document.querySelector('.coating').innerHTML = select.coating
     }
 }
 
-let btn_save = document.querySelector('.btn_save')
+
 liste.addEventListener('input', num_one)
 btn_save.addEventListener('click', save)
 
